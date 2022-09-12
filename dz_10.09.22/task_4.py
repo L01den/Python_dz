00e7333eca9adea17fd41dfffe327314cb15b23c
Дарья Родина <105981793+L01den@users.noreply.github.com>
2022-09-12 16:10:15 +0300

    задание 4

--- dz_10.09.22/task_4.py
+++ dz_10.09.22/task_4.py
@@ -3,3 +3,13 @@
 # - 45 -> 101101
 # - 3 -> 11
 # - 2 -> 10
+
+def binary_number(number):
+    str_num = ''
+    while number != 0:
+        str_num += (str(number % 2))
+        number //= 2
+    bin_num = str_num[::-1]
+    return bin_num
+
+print(binary_number(23))
