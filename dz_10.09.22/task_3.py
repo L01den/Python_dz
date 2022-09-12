fb8e74fecca91c3f8e88c1c21d3879bb43e4fd82
Дарья Родина <105981793+L01den@users.noreply.github.com>
2022-09-12 15:22:46 +0300

    задание 3

--- dz_10.09.22/task_3.py
+++ dz_10.09.22/task_3.py
@@ -2,3 +2,16 @@
 # между максимальным и минимальным значением дробной части элементов.
 # Пример:
 # - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+
+def diff_min_max(input_list, output_list):
+    for i in range(len(input_list)):
+        value = round(input_list[i] % 1, 3)
+        if value != 0:
+            output_list.append(value)
+    
+    return (max(output_list) - min(output_list))
+
+my_list = [1.1, 1.2, 3.1, 5, 10.01]
+answer_list = []
+
+print(diff_min_max(my_list, answer_list))
