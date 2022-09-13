e4e60ebda42252a3e35444de776c47f09382a60f
Дарья Родина <105981793+L01den@users.noreply.github.com>
2022-09-13 15:00:49 +0300

    Update task_5.py

--- dz_10.09.22/task_5.py
+++ dz_10.09.22/task_5.py
@@ -4,57 +4,6 @@
 # Fn = F(n-1) + F(n-2)
 # негафибоначи Fn = F(n+2)−F(n+1)
 
-# fib_list = []
-# i = 0
-# n = 8
-# while i < n:
-#     if i == 0:
-#         fib_list.append(0)
-#         fib_list.append(1)
-#         i += 2
-#     else:
-#         fib_list.append((fib_list[i - 1]) + (fib_list[i - 2]))
-#         i += 1
-        
-# print(fib_list)
-# print(n)
-
-
-def fibonacci(fib1, fib2):
-    print(fib1, fib2, end=' ')
-    for i in range(2, n):
-        fib_sum = fib1 + fib2
-        fib1 = fib2
-        fib2 = fib_sum
-        print(fib2, end=' ')
-
-fib1 = 0
-fib2 = 1
-n = int(input('Введите число '))
-
-fibonacci(fib1, fib2)
-
-
-
-
-def fibonacci(n):
-    if n in [1, 2]:
-        return 1
-    else:
-        return fibonacci(n - 1) + fibonacci(n - 2)
-
-n = int(input('Введите число '))
-my_list = []
-for j in range(1, n):
-    my_list.append(fibonacci(j))
-print(my_list)
-
-
-
-
-
-
-
 def fibonacci(n):
     if n in [1, 2]:
         return 1
@@ -71,9 +20,10 @@ for j in range(1, n + 1):
     else:    
         negariv_fib.append(fibonacci(j))
 negariv_fib.reverse()
+negariv_fib.append(0)
     
 for j in range(1, n + 1):
     normal_fib.append(fibonacci(j))
     
-print(normal_fib)
+negariv_fib.extend(normal_fib)
 print(negariv_fib)
