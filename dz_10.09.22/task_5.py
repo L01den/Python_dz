1612897b0a883cbe2b1d5e02e52cf371858cb0f8
Дарья Родина <105981793+L01den@users.noreply.github.com>
2022-09-13 13:29:55 +0300

    правка дз

--- dz_10.09.22/task_5.py
+++ dz_10.09.22/task_5.py
@@ -4,17 +4,32 @@
 # Fn = F(n-1) + F(n-2)
 # негафибоначи Fn = F(n+2)−F(n+1)
 
-fib_list = []
-i = 0
-n = 8
-while i < n:
-    if i == 0:
-        fib_list.append(0)
-        fib_list.append(1)
-        i += 2
-    else:
-        fib_list.append((fib_list[i - 1]) + (fib_list[i - 2]))
-        i += 1
+# fib_list = []
+# i = 0
+# n = 8
+# while i < n:
+#     if i == 0:
+#         fib_list.append(0)
+#         fib_list.append(1)
+#         i += 2
+#     else:
+#         fib_list.append((fib_list[i - 1]) + (fib_list[i - 2]))
+#         i += 1
         
-print(fib_list)
-print(n)
+# print(fib_list)
+# print(n)
+
+
+def fibonacci(fib1, fib2):
+    print(fib1, fib2, end=' ')
+    for i in range(2, n):
+        fib_sum = fib1 + fib2
+        fib1 = fib2
+        fib2 = fib_sum
+        print(fib2, end=' ')
+
+fib1 = 0
+fib2 = 1
+n = int(input('Введите число '))
+
+fibonacci(fib1, fib2)
