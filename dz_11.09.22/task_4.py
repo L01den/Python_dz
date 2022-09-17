ffe609cd181c44fcd27f92ce4f63e26cea87b7bf
Daria <procionestrisce@yandex.ru>
2022-09-17 17:54:29 +0300

    Новая ветка

--- dz_11.09.22/task_4.py
+++ dz_11.09.22/task_4.py
@@ -4,25 +4,30 @@
 # k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 # 2, 4, 5 - генерятся рандомно
 # стперень x - задаёт пользователь и она уменьшаеться
-from random import randint
+import random 
 
 def random_factor(k, min = 0, max = 101) -> list:
     for i in range(k+1):
         my_list.append(random.randint(min, max))
     return my_list
     
-# def equality(my_list, k):
-#     for i in range(len(my_list):
+def equality(k):
+    for i in range(len(my_list)):
+        if i!= (len(my_list) - 1):
+            output_lits.insert(i, (f'{my_list[i]}*x^{k} +'))
+            k -= 1
+        else:
+            output_lits.insert(i, (f'{my_list[i]} = 0'))
+    return output_lits
         
-
 k = int(input('Введите степень числа '))
-my_list = []
+my_list = []  
+output_lits = []
     
-    
-print(random_factor(k))
+random_factor(k)
 
-my_list.insert(0, (f'{my_list[0]}x^{k} +'))
-
+with open('Homeworke/dz_11.09.22/answer.txt', 'w') as an:
+    an.write(' '.join(equality(k)))
 
 
 
--- dz_11.09.22/task_5.py
+++ dz_11.09.22/task_5.py
@@ -1,2 +1,21 @@
 # 5.Даны два файла, в каждом из которых находится запись многочлена.
 # Задача - сформировать файл, содержащий сумму многочленов.
+
+# with open('Homeworke/dz_11.09.22/example_1.txt', 'w') as e_1:
+#     e_1.write('4x - 1 + 2x')
+
+with open('Homeworke/dz_11.09.22/example_1.txt', 'r') as e_1:
+    e1_list = str(e_1.readline())
+    # print(e1_list)
+
+
+# with open('Homeworke/dz_11.09.22/example_2.txt', 'w') as e_2:
+#     e_2.write('5 + 3x - 1x')
+
+with open('Homeworke/dz_11.09.22/example_2.txt', 'r') as e_2:
+    e2_list = str(e_2.readline())
+    # print(e2_list)
+
+answer = f'{e1_list} + {e2_list}'
+
+print(type(answer))
