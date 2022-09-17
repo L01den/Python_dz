f1ab454516703c4c2fb51e7ca146fd9053d52023
Daria <procionestrisce@yandex.ru>
2022-09-17 17:59:41 +0300

    Заданеи 4

--- dz_11.09.22/task_5.py
+++ dz_11.09.22/task_5.py
@@ -1,2 +1,17 @@
 # 5.Даны два файла, в каждом из которых находится запись многочлена.
 # Задача - сформировать файл, содержащий сумму многочленов.
+
+with open('Homeworke/dz_11.09.22/example_1.txt', 'r') as e_1:
+    e1_list = str(e_1.readline())
+
+with open('Homeworke/dz_11.09.22/example_2.txt', 'r') as e_2:
+    e2_list = str(e_2.readline())
+
+answer = f'{e1_list} + {e2_list}'
+
+
+print(''.join([word for word in answer if 'x' not in word]))
+
+
+
+# print(type(answer))
