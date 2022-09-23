0d7cee3f6a5c25d11336fc3445f0bb1f99832277
Дарья Родина <105981793+L01den@users.noreply.github.com>
2022-09-23 11:24:30 +0300

    Update game_2021_smart_bot.py

--- dz_17.09.22/game_2021_smart_bot.py
+++ dz_17.09.22/game_2021_smart_bot.py
@@ -21,20 +21,35 @@ name_2 = 'Бот'
 player_1 = 0
 candy = 136
 
-while candy > 0:
-    if candy > 0:
-        xod = error_checking(player_1, name_1)
-        candy -= xod
-        name = name_1
-    if candy > 21:
-        player_2 = 29 - xod
-        print(f'Ходит бот: {player_2}')
-        candy -= player_2
-        name = name_2
-    elif candy > 0:
-        player_2 = 21
-        print(f'Ходит бот: {player_2}')
-        candy -= player_2
-        name = name_2
+lot = random.randint(0, 1)
+if lot == 1:
+    while candy > 0:
+        if candy > 0:
+            xod = error_checking(player_1, name_1)
+            candy -= xod
+            name = name_1
+        if candy > 21:
+            player_2 = 29 - xod
+            print(f'Ходит бот: {player_2}')
+            candy -= player_2
+            name = name_2
+        elif candy > 0:
+            player_2 = 21
+            print(f'Ходит бот: {player_2}')
+            candy -= player_2
+            name = name_2
+else:
+    print(f'Ходит бот: 20')
+    candy -= 20
+    while candy > 0:
+        if candy > 0:
+            xod = error_checking(player_1, name_1)
+            candy -= xod
+            name = name_1
+        if candy > 0:
+            player_2 = 29 - xod
+            print(f'Ходит бот: {player_2}')
+            candy -= player_2
+            name = name_2
 
 print(f'Победитель {name}!!!')
