dd5d3e4028436329ef8e97278a3cc81103dc5c58
Daria <procionestrisce@yandex.ru>
2022-09-25 00:52:51 +0300

    Дз 6

--- dz_18.09.22/task_1.py
+++ dz_18.09.22/task_1.py
@@ -4,6 +4,6 @@ n = int(input('Введите число N: '))
 new_list = [((1 + 1/i)**i) for i in (map(int, range(1, n+1)))]  # решение в 3 строки
 print(new_list)
 
-# print([((1 + 1/i)**i) for i in (map(int, range(1, n+1)))])    # и решение в 2 строки
+
 
 
